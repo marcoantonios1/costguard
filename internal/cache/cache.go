8bd3b753fe52e2df32460b121888162d11e91a7a
Marco <marco_antonios1@outlook.com>
2026-03-06 11:55:11 +0200

    Define cache Entry struct and Cache interface for memory caching implementation

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -1,1 +1,15 @@
 package cache
+
+import "time"
+
+type Entry struct {
+	StatusCode int
+	Header     map[string][]string
+	Body       []byte
+	ExpiresAt  time.Time
+}
+
+type Cache interface {
+	Get(key string) (Entry, bool)
+	Set(key string, entry Entry)
+}
